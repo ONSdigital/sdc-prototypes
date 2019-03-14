7f8d7b82538562b9b77ab16b11fb52067365b87d
Ben Meyrick <bameyrick@gmail.com>
2019-03-14 15:46:18 +0000

    Comment out some files to see if netlify will build

--- src/prototypes/address-lookup/scripts/index.js
+++ src/prototypes/address-lookup/scripts/index.js
@@ -1,12 +1,12 @@
 import 'helpers/previous-link';
 import '../components/typeahead/typeahead';
 import '../components/address-input/address-input';
-import './questions-manager';
-import SummaryManager from './summary-manager';
-import summaryTemplate from '!nunjucks-loader!../views/partials/_summary.njk';
+// import './questions-manager';
+// import SummaryManager from './summary-manager';
+// import summaryTemplate from '!nunjucks-loader!../views/partials/_summary.njk';
 
-const summaryPlaceholder = document.querySelector('.js-summary');
+// const summaryPlaceholder = document.querySelector('.js-summary');
 
-if (summaryPlaceholder) {
-  new SummaryManager(summaryPlaceholder, summaryTemplate);
-}
+// if (summaryPlaceholder) {
+//   new SummaryManager(summaryPlaceholder, summaryTemplate);
+// }
