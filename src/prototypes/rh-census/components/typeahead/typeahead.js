7d993c79faa5efd75655661484f22b9f01635dd7
Ben Meyrick <bameyrick@gmail.com>
2019-06-06 09:23:53 +0100

    Update web-chat prototype to have prototype address input

---
+++ src/prototypes/rh-census/components/typeahead/typeahead.js
@@ -0,0 +1,35 @@
+import domReady from 'helpers/domready';
+import TypeaheadCore from '../typeahead/typeahead-core';
+
+class Typeahead {
+  constructor(context) {
+    this.context = context;
+    this.typeahead = new TypeaheadCore({
+      context,
+      onSelect: this.onSelect.bind(this),
+      onUnsetResult: this.onUnsetResult.bind(this)
+    });
+
+    this.code = context.querySelector('.js-typeahead-code');
+  }
+
+  onSelect(result) {
+    return new Promise(resolve => {
+      this.code.value = result.code;
+
+      resolve();
+    });
+  }
+
+  onUnsetResult() {
+    this.code.value = '';
+  }
+}
+
+function typeaheads() {
+  const typeaheads = [...document.querySelectorAll('.js-typeahead')];
+
+  typeaheads.forEach(typeahead => new Typeahead(typeahead));
+}
+
+domReady(typeaheads);
--- src/prototypes/rh-census/index.js
+++ src/prototypes/rh-census/index.js
@@ -1,6 +1,10 @@
 import domReady from 'helpers/domready';
 
 import UACEntry from './components/uac-entry/uac-entry.dom';
+import './components/address-input/address-input';
+import './scripts/questions-manager';
+import './scripts/address-confirmer';
+import './scripts/piping';
 
 domReady(() => {
   UACEntry();
