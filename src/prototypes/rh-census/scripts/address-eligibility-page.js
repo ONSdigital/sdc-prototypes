ad790d600a21f635e306558969be666653e1d6cb
Ben Meyrick <bameyrick@gmail.com>
2019-06-28 09:51:43 +0100

    Added no-js variant of request uac journey

--- src/prototypes/rh-census/scripts/address-eligibility-page.js
+++ src/prototypes/rh-census/scripts/address-eligibility-page.js
@@ -8,6 +8,7 @@ function actionChanger() {
     const originalAction = form.action;
     const selectedFromLookupInput = document.querySelector('.js-address-from-lookup');
     const lookupAction = pathname.replace('/address.html', '/no-js-address-lookup.html');
+    const urlParams = new URLSearchParams(window.location.search);
 
     selectedFromLookupInput.addEventListener('change', () => {
       if (selectedFromLookupInput.value === 'true') {
@@ -16,6 +17,25 @@ function actionChanger() {
         form.action = lookupAction;
       }
     });
+
+    if (urlParams.get('no-js') === 'true') {
+      sessionStorage.setItem('no-js', true);
+    }
+
+    if (urlParams.get('no-js') === 'false') {
+      sessionStorage.removeItem('no-js');
+    }
+    if (sessionStorage.getItem('no-js')) {
+      const manualButton = document.querySelector('.js-address-manual-btn');
+
+      setTimeout(() => {
+        if (document.querySelector('.js-address-from-lookup').value === 'true') {
+          manualButton.click();
+        }
+
+        document.querySelector('.js-address-search-btn-container').classList.add('u-d-no');
+      }, 50);
+    }
   }
 }
 
