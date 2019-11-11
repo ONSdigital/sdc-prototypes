c5078afe9d4e6d5a9efc417139ff017f9d7be4dc
jrbarnes9 <jrbarnes9@hotmail.com>
2019-11-11 14:28:26 +0000

    updated v2 with visible write ins

--- src/prototypes/voluntary-radios-v2/components/radios/radios.js
+++ src/prototypes/voluntary-radios-v2/components/radios/radios.js
@@ -1,24 +1,33 @@
 class Radios {
   constructor(context) {
     this.inputs = [...context.querySelectorAll('.js-radio')];
-    this.clearButton = [...document.querySelectorAll('.btn-clear')];
+    this.button = [...document.querySelectorAll('.btn-clear')];
     this.submitButton = [...document.querySelectorAll('button[type="submit"]')];
-
+    this.otherField = [document.querySelector('#other-textbox')];
+    this.otherFieldRadio = [...document.querySelectorAll('.voluntary')];
     this.inputs.forEach(input => input.addEventListener('click', this.showClearBtn.bind(this)));
-    this.clearButton.forEach(input => input.addEventListener('click', this.clearRadios.bind(this)));
+    this.button.forEach(input => input.addEventListener('click', this.clearRadios.bind(this)));
+    this.otherField.forEach(input => input.addEventListener('click', this.checkRadio.bind(this)));
+    this.otherFieldRadio.forEach(input => input.addEventListener('click', this.checkRadio.bind(this)));
+    this.button.forEach(input => input.classList.add('u-vh'));
   }
 
   showClearBtn() {
-    this.clearButton.forEach(input => input.classList.remove('u-vh'));
-    this.submitButton.forEach(input => input.classList.remove('u-mt-xl'));
-    this.submitButton.forEach(input => input.classList.add('u-mt-no'));
+    this.button.forEach(button => button.classList.remove('u-vh'));
+    this.otherField.forEach(input => (input.value = ''));
   }
 
-  clearRadios() {
+  clearRadios(event) {
+    event.preventDefault();
     this.inputs.forEach(input => (input.checked = false));
-    this.clearButton.forEach(input => input.classList.add('u-vh'));
-    this.submitButton.forEach(input => input.classList.remove('u-mt-no'));
-    this.submitButton.forEach(input => input.classList.add('u-mt-xl'));
+    this.otherField.forEach(input => (input.value = ''));
+    this.button.forEach(input => input.classList.add('u-vh'));
+  }
+
+  checkRadio() {
+    this.otherFieldRadio.forEach(input => (input.checked = true));
+    this.button.forEach(input => input.classList.remove('u-vh'));
+    this.otherField.forEach(input => input.focus());
   }
 }
 
