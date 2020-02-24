7f4ed426c214a64d3f29b17a2426b81d41aa1a7a
boxadesign <info@boxadesign.co.uk>
2020-02-24 16:14:13 +0000

    Updated video component

--- src/prototypes/bsl/scripts/index.js
+++ src/prototypes/bsl/scripts/index.js
@@ -1,18 +1,30 @@
-const button = document.querySelector('.js-bsl-btn');
-if (button) {
+const urlParams = new URLSearchParams(window.location.search);
+const bsl = urlParams.has('bsl');
+
+if (bsl) {
+  const button = document.querySelector('.js-bsl-btn');
   const buttonInner = button.querySelector('.btn__inner');
+  const videoWrap = document.querySelector('.bsl-video');
   const video = document.querySelector('.bsl-video__video');
+
+  const form = document.getElementsByTagName('form')[0];
   let open = true;
+
+  form.action = form.action + '?bsl';
+
+  videoWrap.classList.remove('u-d-no');
   button.addEventListener('click', toggleVideo.bind(this));
 
   function toggleVideo(e) {
     e.preventDefault();
     if (open === true) {
       video.classList.add('u-d-no');
+      buttonInner.classList.add('icon--bsl-btn--closed');
       buttonInner.textContent = 'Show BSL';
       open = false;
     } else {
       video.classList.remove('u-d-no');
+      buttonInner.classList.remove('icon--bsl-btn--closed');
       buttonInner.textContent = 'Hide BSL';
       open = true;
     }
