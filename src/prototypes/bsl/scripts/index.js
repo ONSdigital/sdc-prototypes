43c7f9917e1926df5d95b70518965348aa911099
boxadesign <info@boxadesign.co.uk>
2020-02-25 10:45:11 +0000

    Updated behaviour

--- src/prototypes/bsl/scripts/index.js
+++ src/prototypes/bsl/scripts/index.js
@@ -1,42 +1,53 @@
-const bslLink = document.querySelector('.js-bsl-activate');
+import domReady from 'helpers/domready';
 
-bslLink.addEventListener('click', activateBSL.bind(this));
-function activateBSL() {
-  window.sessionStorage.setItem('bsl', true);
-}
+function loadBSL() {
+  const bslLink = document.querySelector('.js-bsl-activate');
 
-const bsl = window.sessionStorage.getItem('bsl');
+  bslLink.addEventListener('click', setBSL.bind(this));
+  function setBSL() {
+    window.sessionStorage.setItem('bsl', true);
+  }
 
-if (bsl) {
-  const button = document.querySelector('.js-bsl-btn');
-  const buttonInner = button.querySelector('.btn__inner');
-  const videoWrap = document.querySelector('.bsl-video');
-  const video = document.querySelector('.bsl-video__video');
+  const bsl = window.sessionStorage.getItem('bsl');
 
-  const form = document.getElementsByTagName('form')[0];
-  const formInputs = [...form.getElementsByTagName('input')];
-  let open = true;
+  if (bsl) {
+    const button = document.querySelector('.js-bsl-btn');
+    const buttonInner = button.querySelector('.btn__inner');
+    const videoWrap = document.querySelector('.bsl-video');
+    const video = videoWrap.querySelector('.bsl-video__video');
+    const form = document.getElementsByTagName('form')[0];
+    const formInputs = [...form.getElementsByTagName('input')];
+    let open = true;
 
-  form.action = form.action + '?bsl';
+    initialiseBSL();
 
-  videoWrap.classList.remove('u-d-no');
-  button.addEventListener('click', toggleVideo.bind(this));
-  formInputs.forEach(input => {
-    const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    if (vw < 960) {
-      input.addEventListener('focus', toggleVideo.bind(this));
+    function initialiseBSL() {
+      videoWrap.classList.remove('u-d-no');
+
+      button.addEventListener('click', toggleVideo.bind(this));
+
+      formInputs.forEach(input => {
+        const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        if (vw < 960) {
+          input.addEventListener('focus', toggleVideo.bind(this));
+        }
+      });
     }
-  });
 
-  function toggleVideo(event) {
-    event.preventDefault();
-    if (open === true || event.type === 'focus') {
+    function toggleVideo(event) {
+      event.preventDefault();
+      open === true || event.type === 'focus' ? hide() : show();
+    }
+
+    function hide() {
       video.classList.add('u-d-no');
       videoWrap.classList.add('bsl-video--closed');
       buttonInner.classList.add('icon--bsl-btn--closed');
       buttonInner.textContent = 'Show BSL';
       open = false;
-    } else {
+    }
+
+    function show() {
       video.classList.remove('u-d-no');
       videoWrap.classList.remove('bsl-video--closed');
       buttonInner.classList.remove('icon--bsl-btn--closed');
@@ -45,3 +56,5 @@ if (bsl) {
     }
   }
 }
+
+domReady(loadBSL);
