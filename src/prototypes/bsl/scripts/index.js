2e1935a82e0772cdaf6ac38a050d364bcbde1372
boxadesign <info@boxadesign.co.uk>
2020-02-25 00:24:30 +0000

    Added session storage to persist bsl

--- src/prototypes/bsl/scripts/index.js
+++ src/prototypes/bsl/scripts/index.js
@@ -1,5 +1,11 @@
-const urlParams = new URLSearchParams(window.location.search);
-const bsl = urlParams.has('bsl');
+const bslLink = document.querySelector('.js-bsl-activate');
+
+bslLink.addEventListener('click', activateBSL.bind(this));
+function activateBSL() {
+  window.sessionStorage.setItem('bsl', true);
+}
+
+const bsl = window.sessionStorage.getItem('bsl');
 
 if (bsl) {
   const button = document.querySelector('.js-bsl-btn');
@@ -8,16 +14,23 @@ if (bsl) {
   const video = document.querySelector('.bsl-video__video');
 
   const form = document.getElementsByTagName('form')[0];
+  const formInputs = [...form.getElementsByTagName('input')];
   let open = true;
 
   form.action = form.action + '?bsl';
 
   videoWrap.classList.remove('u-d-no');
   button.addEventListener('click', toggleVideo.bind(this));
+  formInputs.forEach(input => {
+    const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    if (vw < 960) {
+      input.addEventListener('focus', toggleVideo.bind(this));
+    }
+  });
 
-  function toggleVideo(e) {
-    e.preventDefault();
-    if (open === true) {
+  function toggleVideo(event) {
+    event.preventDefault();
+    if (open === true || event.type === 'focus') {
       video.classList.add('u-d-no');
       videoWrap.classList.add('bsl-video--closed');
       buttonInner.classList.add('icon--bsl-btn--closed');
