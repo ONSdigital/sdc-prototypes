54061138241090bbbd09204479b185cf1431dd24
jrbarnes9 <jrbarnes9@hotmail.com>
2021-08-09 16:20:55 +0100

    fixed script issue in self serve proto

--- src/prototypes/user-self-serve/version-9/scripts/index.js
+++ src/prototypes/user-self-serve/version-9/scripts/index.js
@@ -1,11 +1,3 @@
 import './questions-manager';
 import './piping';
 import './previous-link';
-import SummaryManager from './summary-manager';
-import summaryTemplate from '!nunjucks-loader!../../views/partials/_summary.njk';
-
-const summaryPlaceholder = document.querySelector('.js-summary');
-
-if (summaryPlaceholder) {
-  new SummaryManager(summaryPlaceholder, summaryTemplate);
-}
